6a9a6ea434fd8be45defef80ad96516eae789f51
Kaique Leonardo <kaiqueleonardo18@gmail.com>
2023-08-08 13:57:48 -0300

    first commit

---
+++ Infraestrutura/Models/Noticia.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Infraestrutura.Models
+{
+    public class Noticia
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public string Chapeu { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        [Required]
+        public string Autor { get; set; }
+    }
+}
